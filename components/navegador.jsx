3c9685c92ca8bbdae183d6ebeab18080357be31e
Julio Pagnussat <juliopagnussat@MacBook-Pro-de-Julio.local>
2021-12-17 14:19:55 -0300

    alteracao1

---
+++ components/navegador.jsx
@@ -0,0 +1,7 @@
+export default function Navegador () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+export default function Home () {
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '50vh'
+        }}>
+            <h1>Página em Construção!</h1>
+        </div>
+    )
+}
