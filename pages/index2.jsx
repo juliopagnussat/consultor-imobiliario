18623e2f4afd8e71e96092cb88cb75e20b9e1cd7
Julio Pagnussat <juliopagnussat@MacBook-Pro-de-Julio.local>
2021-12-17 15:15:31 -0300

    alteracao4

--- pages/index2.jsx
+++ pages/index2.jsx
@@ -5,7 +5,7 @@ export default function Home () {
     return (
             <div>
             <Navegador texto="Tour Imóveis" destino="/videos"/>
-            <Navegador texto="Encontre seu imóvel" destino="http://bit.ly/whats_juliopagnussat"/>
+            <Navegador texto="Encontre seu imóvel" destino="/imoveis"/>
             <Navegador texto="Whatsapp - (49) 99115-2221" destino="http://bit.ly/whats_juliopagnussat"/>
             </div>
     )
